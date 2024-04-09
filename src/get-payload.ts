bc5a5e1e32ba0a63759a72ac9e546b1fac49c347
Yash-jaiswal2509 <yashjaiswal2509@gmail.com>
2024-04-09 09:35:05 +0530

    admin dashboard done

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,3 +1,37 @@
 import "dotenv/config";
+import payload from "payload";
+import type { InitOptions } from 'payload/config'
 
-export const getPayloadClient = async () => {};
+let cached = (global as any).payload;
+
+if (!cached) {
+  cached = (global as any).payload = {
+    client: null,
+    promise: null,
+  };
+}
+
+interface Args {
+  initOptions?: Partial<InitOptions>;
+}
+
+export const getPayloadClient = async ({ initOptions }: Args) => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error("PAYLOAD_SECRET is missing");
+  }
+
+  if (!cached.promise) {
+    cached.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    });
+  }
+
+  try {
+    cached.client = await cached.promise;
+  } catch (error: unknown) {
+    cached.promise = null;
+    throw error;
+  }
+};
